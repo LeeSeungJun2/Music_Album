15041de1b35078fba40da285d4215a81fff7eefd
JS-404-000 <jindate2@gmail.com>
2025-04-16 11:19:56 +0900

    commit

--- app/src/main/java/com/example/musicalbum/MainActivity.java
+++ app/src/main/java/com/example/musicalbum/MainActivity.java
@@ -1,6 +1,9 @@
 package com.example.musicalbum;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.LinearLayout;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -15,5 +18,15 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
+    }
+
+    public void play(View view) {
+        int id = view.getId();
+        LinearLayout layout = findViewById(id);
+        String tag = (String) layout.getTag();
+        Intent it = new Intent(this, AudioImage.class);
+        it.putExtra("it_tag", tag);
+        startActivity(it);
+
     }
 }
